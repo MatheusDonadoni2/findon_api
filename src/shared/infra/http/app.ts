8a0ed12868b68a09665bf733a223fff2ca1b1a31
matheusdonadoni2 <matheusgoncalvesdonadoni@gmail.com>
2022-10-02 14:11:35 -0300

    Coes

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,10 +2,12 @@ import express, { json, NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import { AppError } from '../../errors/AppError';
 import { routes } from './routes';
+import cors from "cors"
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use(
